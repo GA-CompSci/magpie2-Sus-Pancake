3bb4d21624afaa70224afd01805486122a701061
Sus-Pancake <89539674+Sus-Pancake@users.noreply.github.com>
2024-09-09 14:43:01 +0000

    nevermind

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        print(str:"Hello World!");
-        print("The sum of 5 and 3 is " + add(a:5, b:3));
+        print("Hello World!");
+        print("The sum of 5 and 3 is " + add(5, 3));
     }
     public static void print(String str){
         System.out.println(str);
     }
     public static int add(int a, int b){
-        
+        return a + b;
     }
 }
