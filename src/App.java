807da91725f0f191e24c9cf9554af0837b375221
Sus-Pancake <89539674+Sus-Pancake@users.noreply.github.com>
2024-09-09 14:38:03 +0000

    Help same problem matthew has

--- src/App.java
+++ src/App.java
@@ -1,10 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        print(str:"Hello World!");
+        print("The sum of 5 and 3 is " + add(a:5, b:3));
+    }
+    public static void print(String str){
+        System.out.println(str);
+    }
+    public static int add(int a, int b){
         
-        String sdfsdf = "sdfsdf";
-
-        System.out.println(sdfsdf.indexOf("sdf") != -1);
-        
-
     }
 }
